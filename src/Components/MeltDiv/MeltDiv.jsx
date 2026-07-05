0976beaef3812b3ca761296f3fd16d4ee1911990
m0h1m3d <mohisham33@gmail.com>
2026-07-05 23:16:03 +0200

    refractor melting component

--- src/Components/MeltDiv/MeltDiv.jsx
+++ src/Components/MeltDiv/MeltDiv.jsx
@@ -2,22 +2,7 @@ import "./meltdiv.scss";
 function MeltDiv({ color }) {
   const pic = `/melt${color}.png` ;
   return (
-    <div className="melt">
-      <div className="meltingbg-mb">
-        <img src={pic} alt="" />
-      </div>
-      <div className="meltingbg desk">
-        <img src={pic} alt="" />
-      </div>
-      <div className="meltingbg desk">
-        <img src={pic} alt="" />
-      </div>
-      <div className="meltingbg desk">
-        <img src={pic} alt="" />
-      </div>
-      <div className="meltingbg desk">
-        <img src={pic} alt="" />
-      </div>
+      <div className="meltingbg" style={{ backgroundImage: `url(${pic})` }} >
     </div>
   );
 }
